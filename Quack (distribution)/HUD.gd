41b942f7995ac6b458e75684c2cc1cdce2ac3b82
ExplodingImplosion <60752903+ExplodingImplosion@users.noreply.github.com>
2021-04-16 15:43:27 -0400

    asdf
    asdf

--- Quack (distribution)/HUD.gd
+++
@@ -1,7 +0,0 @@
-extends ViewportContainer
-
-export onready var ammocount = $Viewport/AmmoCount
-export onready var viewmodel = $Viewport/tempviewmodel
-export onready var health = $Viewport/Health
-export onready var spawntimer = $Viewport/Respawntimer
-export onready var viewport = $Viewport
--- Quack (distribution)/mainmenufuncs.gd
+++
@@ -1,7 +0,0 @@
-extends Tree
-
-func _ready():
-	#$Playbutton
-	$"Stats and goodies".set_text("Stats and goodies")
-	$Options.set_text("Options")
-	#$"Quit game"
